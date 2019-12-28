4bf260ebbac813cb35a633c86b39828349a5a7ee
Park Ju Hyung <qkrwngud825@gmail.com>
2019-12-28 23:56:31 +0900

    Revert "jemalloc: always assume default thp mode"
    This reverts commit f3f4b175aec1ee28360e6c10430667e925f8793b.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- src/pages.c
+++ src/pages.c
@@ -555,9 +555,42 @@ init_thp_state(void) {
 		goto label_error;
 	}
 
-	init_system_thp_mode = thp_mode_default;
-	return;
+	static const char sys_state_madvise[] = "always [madvise] never\n";
+	static const char sys_state_always[] = "[always] madvise never\n";
+	static const char sys_state_never[] = "always madvise [never]\n";
+	char buf[sizeof(sys_state_madvise)];
 
+#if defined(JEMALLOC_USE_SYSCALL) && defined(SYS_open)
+	int fd = (int)syscall(SYS_open,
+	    "/sys/kernel/mm/transparent_hugepage/enabled", O_RDONLY);
+#else
+	int fd = open("/sys/kernel/mm/transparent_hugepage/enabled", O_RDONLY);
+#endif
+	if (fd == -1) {
+		goto label_error;
+	}
+
+	ssize_t nread = malloc_read_fd(fd, &buf, sizeof(buf));
+#if defined(JEMALLOC_USE_SYSCALL) && defined(SYS_close)
+	syscall(SYS_close, fd);
+#else
+	close(fd);
+#endif
+
+        if (nread < 0) {
+		goto label_error; 
+        }
+
+	if (strncmp(buf, sys_state_madvise, (size_t)nread) == 0) {
+		init_system_thp_mode = thp_mode_default;
+	} else if (strncmp(buf, sys_state_always, (size_t)nread) == 0) {
+		init_system_thp_mode = thp_mode_always;
+	} else if (strncmp(buf, sys_state_never, (size_t)nread) == 0) {
+		init_system_thp_mode = thp_mode_never;
+	} else {
+		goto label_error;
+	}
+	return;
 label_error:
 	opt_thp = init_system_thp_mode = thp_mode_not_supported;
 }
